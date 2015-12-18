ff044871d524d5b0ae5dcc2a6b48dd39be6fef05
Jakub Olejník <jakub.olejnik@ackee.cz>
2015-12-18 19:07:18 +0100

    New argo extensions

--- Source/ArgoExtensions.swift
+++ Source/ArgoExtensions.swift
@@ -1,90 +1,120 @@
-//We stopped using argo but lets keep this file in case we ever need to use it again
-////
-////  Argo+RAC.swift
-////  Rekola
-////
-////  Created by Petr Šíma on Jun/24/15.
-////  Copyright (c) 2015 Ackee s.r.o. All rights reserved.
-////
 //
-//import Foundation
-//import ReactiveCocoa
-//import Argo
+//  ArgoExtensions.swift
+//  Pixm8
 //
-//struct ArgoErrors {
-//	static let domain = "ArgoErrorDomain"
-//	static let missingKeyErrorKey = "ArgoMissingKey"
-//	static let typeMismatchErrorKey = "ArgoTypeMismatch"
-//}
+//  Created by Jakub Olejník on 01/12/15.
+//  Copyright © 2015 Ackee s.r.o. All rights reserved.
 //
-//extension Decoded { //Argo errors
-//	var errorCode : Int {
-//		switch self {
-//		case .MissingKey(_): return 1404
-//		case .TypeMismatch(_): return 1400
-//		default: return 0
-//		}
-//	}
-//}
-//
-//public func rac_decode<T: Decodable where T == T.DecodedType>(object: AnyObject) -> SignalProducer<T,NSError> {
-//	return SignalProducer { sink, disposable in
-//		
-//		let decoded : Decoded<T> = decode(object)
-//		switch decoded {
-//		case .Success(let box):
-//			sendNext(sink, box.value)
-//			sendCompleted(sink)
-//		case .MissingKey(let k):
-//			let error = NSError(domain: ArgoErrors.domain, code: decoded.errorCode, userInfo: [ArgoErrors.missingKeyErrorKey : k])
-//			sendError(sink, error)
-//		case .TypeMismatch(let t):
-//			let error = NSError(domain: ArgoErrors.domain, code: decoded.errorCode, userInfo: [ArgoErrors.missingKeyErrorKey : t])
-//			sendError(sink, error)
-//		}
-//		
-//		
-//	}
-//}
-//
-//
-//
-//public func rac_decode<T: Decodable where T == T.DecodedType>(object: AnyObject) -> SignalProducer<[T]?, NSError>  {
-//	return SignalProducer { sink, disposable in
-//		
-//		let decoded : Decoded<[T]> = decode(object)
-//		switch decoded {
-//		case .Success(let box):
-//			sendNext(sink, box.value)
-//			sendCompleted(sink)
-//		case .MissingKey(let k):
-//			let error = NSError(domain: ArgoErrors.domain, code: decoded.errorCode, userInfo: [ArgoErrors.missingKeyErrorKey : k])
-//			sendError(sink, error)
-//		case .TypeMismatch(let t):
-//			let error = NSError(domain: ArgoErrors.domain, code: decoded.errorCode, userInfo: [ArgoErrors.missingKeyErrorKey : t])
-//			sendError(sink, error)
-//		}
-//	}
-//	
-//}
-//
-//public func rac_decodeByOne<T: Decodable where T == T.DecodedType>(object: AnyObject) -> SignalProducer<T, NSError>  {
-//	return SignalProducer { sink, disposable in
-//		
-//		let decoded : Decoded<[T]> = decode(object)
-//		switch decoded {
-//		case .Success(let box):
-//			for value in box.value {
-//				sendNext(sink, value)
-//			}
-//			sendCompleted(sink)
-//		case .MissingKey(let k):
-//			let error = NSError(domain: ArgoErrors.domain, code: decoded.errorCode, userInfo: [ArgoErrors.missingKeyErrorKey : k])
-//			sendError(sink, error)
-//		case .TypeMismatch(let t):
-//			let error = NSError(domain: ArgoErrors.domain, code: decoded.errorCode, userInfo: [ArgoErrors.missingKeyErrorKey : t])
-//			sendError(sink, error)
-//		}
-//
-//	}
-//}
+
+import Foundation
+import ReactiveCocoa
+import Argo
+
+struct ArgoErrors {
+    static let domain = "ArgoErrorDomain"
+    static let missingKeyErrorKey = "ArgoMissingKey"
+    static let typeMismatchErrorActualKey = "ArgoTypeMismatchActual"
+    static let typeMismatchErrorExpectedKey = "ArgoTypeMismatchExpected"
+    static let customErrorExpectedKey = "ArgoCustomError"
+}
+
+extension DecodeError { //Argo errors
+    var errorCode : Int {
+        switch self {
+        case .MissingKey(_): return 1404
+        case .TypeMismatch(_): return 1400
+        default: return 0
+        }
+    }
+}
+
+public func rac_decode<T: Decodable where T == T.DecodedType>(object: AnyObject) -> SignalProducer<T,NSError> {
+    return SignalProducer { sink, disposable in
+        
+        let decoded : Decoded<T> = decode(object)
+        switch decoded {
+        case .Success(let box):
+            sink.sendNext(box)
+            sink.sendCompleted()
+        case .Failure(let e):
+            
+            switch e {
+            case .MissingKey(let k):
+                let error = NSError(domain: ArgoErrors.domain, code: e.errorCode, userInfo: [ArgoErrors.missingKeyErrorKey : k])
+                sink.sendFailed(error)
+                break
+            case .TypeMismatch(let expected, let actual):
+                let error = NSError(domain: ArgoErrors.domain, code: e.errorCode, userInfo: [ArgoErrors.typeMismatchErrorActualKey : expected, ArgoErrors.typeMismatchErrorExpectedKey : actual])
+                sink.sendFailed(error)
+                break
+            case .Custom(let str):
+                let error = NSError(domain: ArgoErrors.domain, code: e.errorCode, userInfo: [ArgoErrors.customErrorExpectedKey : str])
+                sink.sendFailed(error)
+                break
+            }
+        }
+    }
+}
+
+
+
+public func rac_decode<T: Decodable where T == T.DecodedType>(object: AnyObject) -> SignalProducer<[T]?, NSError>  {
+    return SignalProducer { sink, disposable in
+        
+        let decoded : Decoded<[T]> = decode(object)
+        switch decoded {
+        case .Success(let box):
+            sink.sendNext(box)
+            sink.sendCompleted()
+            break
+        case .Failure(let e):
+            
+            switch e {
+            case .MissingKey(let k):
+                let error = NSError(domain: ArgoErrors.domain, code: e.errorCode, userInfo: [ArgoErrors.missingKeyErrorKey : k])
+                sink.sendFailed(error)
+                break
+            case .TypeMismatch(let expected, let actual):
+                let error = NSError(domain: ArgoErrors.domain, code: e.errorCode, userInfo: [ArgoErrors.typeMismatchErrorActualKey : expected, ArgoErrors.typeMismatchErrorExpectedKey : actual])
+                sink.sendFailed(error)
+                break
+            case .Custom(let str):
+                let error = NSError(domain: ArgoErrors.domain, code: e.errorCode, userInfo: [ArgoErrors.customErrorExpectedKey : str])
+                sink.sendFailed(error)
+                break
+            }
+        }
+    }
+    
+}
+
+public func rac_decodeByOne<T: Decodable where T == T.DecodedType>(object: AnyObject) -> SignalProducer<T, NSError>  {
+    return SignalProducer { sink, disposable in
+        
+        let decoded : Decoded<[T]> = decode(object)
+        switch decoded {
+        case .Success(let box):
+            for value in box {
+                sink.sendNext(value)
+            }
+            sink.sendCompleted()
+        case .Failure(let e):
+            
+            switch e {
+            case .MissingKey(let k):
+                let error = NSError(domain: ArgoErrors.domain, code: e.errorCode, userInfo: [ArgoErrors.missingKeyErrorKey : k])
+                sink.sendFailed(error)
+                break
+            case .TypeMismatch(let expected, let actual):
+                let error = NSError(domain: ArgoErrors.domain, code: e.errorCode, userInfo: [ArgoErrors.typeMismatchErrorActualKey : expected, ArgoErrors.typeMismatchErrorExpectedKey : actual])
+                sink.sendFailed(error)
+                break
+            case .Custom(let str):
+                let error = NSError(domain: ArgoErrors.domain, code: e.errorCode, userInfo: [ArgoErrors.customErrorExpectedKey : str])
+                sink.sendFailed(error)
+                break
+            }
+        }
+        
+    }
+}
