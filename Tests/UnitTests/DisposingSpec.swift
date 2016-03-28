8ed2506153a4366f28055782d2d0c6019563574a
Petr Sima <petrsima@Petrs-MacBook-Pro-2.local>
2016-03-28 18:51:54 +0200

    added missing files

---
+++ Tests/UnitTests/DisposingSpec.swift
@@ -0,0 +1,28 @@
+import Quick
+import Nimble
+import ReactiveCocoa
+@testable import SampleTestingProject
+
+class DisposingSpec: QuickSpec {
+    
+    class TestDisposingImpl : Disposing { }
+    
+    override func spec() {
+        describe("disposing") {
+            it("fires an event on its rac_willDeallocSignal when its dealloced") {
+                var disposed = false
+                var objectToDealloc : Disposing? = TestDisposingImpl()
+                SignalProducer<(),NoError> { _, dis in
+                    dis.addDisposable {
+                        disposed = true
+                    }
+                }
+                    .takeUntil(objectToDealloc!.rac_willDeallocSignal)
+                    .start()
+                objectToDealloc = nil
+                
+                expect(disposed).to(beTrue())
+            }
+        }
+    }
+}
