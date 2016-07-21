1c1195befc38231aa0048d23eff7bf9662c84d79
Boris Anisimov <muz1243@gmail.com>
2016-07-21 18:08:53 +0400

    added @testable

--- fastlane/userdata/FileTemplates/Quick/Quick Spec Class.xctemplate/Swift/___FILEBASENAME___.swift
+++ fastlane/userdata/FileTemplates/Quick/Quick Spec Class.xctemplate/Swift/___FILEBASENAME___.swift
@@ -1,8 +1,11 @@
 import Quick
 import Nimble
 
+@testable import ___PROJECTNAME___
 class ___FILEBASENAMEASIDENTIFIER___: QuickSpec {
-    override func spec() {
-
-    }
+	
+	override func spec() {
+		
+	}
+	
 }
