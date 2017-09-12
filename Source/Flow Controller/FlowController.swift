2a1f435aa4b8bc1e1b664704a43d3c9355567209
Lukáš Hromadník <lukas.hromadnik@ackee.cz>
2017-09-12 12:23:22 +0200

    Add missing files

--- Source/Flow Controller/FlowController.swift
+++ Source/Flow Controller/FlowController.swift
@@ -1,44 +1,22 @@
 //
 //  FlowController.swift
-//  Kosik
+//  ProjectSkeleton
 //
-//  Created by Tomas Kohout on 09/06/2017.
+//  Created by Lukáš Hromadník on 12.09.17.
 //  Copyright © 2017 Ackee s.r.o. All rights reserved.
 //
 
-import Foundation
-
-protocol FlowController: class, RoutingProvider {
-    var childControllers: [FlowController] {get set}
+protocol FlowController: class {
+    var children: [FlowController] { get set }
     
-    func addChild(_ flowController: FlowController)
-
-    func handle(action: RoutingAction)
-    
+    func start()
 }
 
 extension FlowController {
-    func addChild(_ flowController: FlowController) {
-        if !childControllers.contains { $0 === flowController } {
-            childControllers.append(flowController)
-        }
-    }
     
-    func removeChild(_ flowController: FlowController) {
-        if let index = childControllers.index (where: { $0 === flowController }) {
-            childControllers.remove(at: index)
-        }
+    func add(child flowController: FlowController) {
+        children.append(flowController)
+        flowController.start()
     }
-    
-    func handle(action: RoutingAction) {
-        
-    }
-    
-    func dispatch(action: RoutingAction) {
-        handle(action: action)
-        childControllers.forEach { (flowController) in
-            flowController.dispatch(action: action)
-        }
-    }
-    
+
 }
