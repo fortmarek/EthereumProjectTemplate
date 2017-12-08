7500ec4ddce37173134b96f925068f6149885a48
Jakub Olejník <jakub.olejnik@ackee.cz>
2017-12-08 17:54:16 +0100

    Add testing app delegate

---
+++ Skeleton/main.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+let appDelegateClass: AnyClass? = NSClassFromString("UnitTests.TestingAppDelegate") ?? AppDelegate.self
+
+let args = UnsafeMutableRawPointer(CommandLine.unsafeArgv).bindMemory(
+    to: UnsafeMutablePointer<Int8>.self,
+    capacity: Int(CommandLine.argc)
+)
+
+UIApplicationMain(CommandLine.argc, args, nil, NSStringFromClass(appDelegateClass!))
---
+++ Tests/Unit/TestingAppDelegate.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+class UnitTestsAppDelegate: NSObject, UIApplicationDelegate {
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey : Any]? = nil) -> Bool {
+        return true
+    }
+}
