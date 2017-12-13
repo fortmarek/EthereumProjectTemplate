7608dd91f2c2c8bac5ab834025a88666a53ef1f2
Jakub Olejník <jakub.olejnik@ackee.cz>
2017-12-13 15:09:16 +0100

    Fix preprocess header generation

--- Tools/generate_preprocess_header.sh
+++ Tools/generate_preprocess_header.sh
@@ -5,5 +5,5 @@ COMMITS=`git rev-list HEAD --count`
 
 echo "// Auto-generated" > "$ACK_ENV_PREPROCESS_HEADER"
 echo "" >> "$ACK_ENV_PREPROCESS_HEADER"
-echo "#define ACK_BUILD_NUMBER \"$COMMITS\"" >> "$ACK_ENV_PREPROCESS_HEADER"
+echo "#define ACK_BUILD_NUMBER $COMMITS" >> "$ACK_ENV_PREPROCESS_HEADER"
 cd -
