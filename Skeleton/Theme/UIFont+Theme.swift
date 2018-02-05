0c8b0d00381ffe6b202daaddcfd745887954e158
Jakub Olejník <jakub.olejnik@ackee.cz>
2018-02-05 15:36:01 +0100

    Add font loading to UIFont+Theme

--- Skeleton/Theme/UIFont+Theme.swift
+++ Skeleton/Theme/UIFont+Theme.swift
@@ -2,6 +2,20 @@ import UIKit
 
 extension UIFont: ThemeProvider { }
 
+// Load fonts when first used from bundle root directory
+private final class BundleLocator {}
+private let fonts: [URL] = {
+    // swiftlint:disable:next force_unwrapping
+    let bundleURL = Bundle(for: BundleLocator.self).resourceURL!
+    let resourceURLs = try? FileManager.default.contentsOfDirectory(at: bundleURL, includingPropertiesForKeys: [], options: .skipsHiddenFiles)
+    let fonts = resourceURLs?.filter { $0.pathExtension == "otf" || $0.pathExtension == "ttf" } ?? []
+    
+    fonts.forEach { CTFontManagerRegisterFontsForURL($0 as CFURL, CTFontManagerScope.process, nil) }
+    
+    return fonts
+}()
+
+
 private protocol FontStyle {
     static func regular(_ size: CGFloat) -> UIFont
     static func bold(_ size: CGFloat) -> UIFont
@@ -9,10 +23,12 @@ private protocol FontStyle {
 
 extension Theme where Base: UIFont {
     static func regular(_ size: CGFloat) -> UIFont {
+        _ = fonts
         return UIFont.systemFont(ofSize: size, weight: .regular)
     }
 
     static func bold(_ size: CGFloat) -> UIFont {
+        _ = fonts
         return UIFont.systemFont(ofSize: size, weight: .bold)
     }
 }
