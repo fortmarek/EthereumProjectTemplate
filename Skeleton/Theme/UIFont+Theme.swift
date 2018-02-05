0407bef00b72ebdfe436976060f5409ff7aef538
Jakub Olejník <jakub.olejnik@ackee.cz>
2018-02-05 15:57:28 +0100

    ACKategories version

--- Skeleton/Theme/UIFont+Theme.swift
+++ Skeleton/Theme/UIFont+Theme.swift
@@ -9,12 +9,11 @@ private let fonts: [URL] = {
     let bundleURL = Bundle(for: BundleLocator.self).resourceURL!
     let resourceURLs = try? FileManager.default.contentsOfDirectory(at: bundleURL, includingPropertiesForKeys: [], options: .skipsHiddenFiles)
     let fonts = resourceURLs?.filter { $0.pathExtension == "otf" || $0.pathExtension == "ttf" } ?? []
-    
+
     fonts.forEach { CTFontManagerRegisterFontsForURL($0 as CFURL, CTFontManagerScope.process, nil) }
-    
+
     return fonts
 }()
-
 
 private protocol FontStyle {
     static func regular(_ size: CGFloat) -> UIFont
