858e3b3f7ead1701cb403a859a58f91d57b60aad
Jakub Olejník <jakub.olejnik@ackee.cz>
2018-06-13 13:50:50 +0200

    Update MainAppDelegate.swift

--- Skeleton/AppDelegates/MainAppDelegate.swift
+++ Skeleton/AppDelegates/MainAppDelegate.swift
@@ -8,8 +8,8 @@ final class MainAppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         window = UIWindow(frame: UIScreen.main.bounds)
-        appFlowController.start()
         window?.makeKeyAndVisible()
+        appFlowController.start()
         return true
     }
 }
