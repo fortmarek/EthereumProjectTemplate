01c9a9784c932ddf022817587f8a3df61a6e7296
pteasima <ptea.sima@gmail.com>
2018-06-20 21:59:28 +0200

    working net_version example against local geth node

--- Skeleton/ViewController/AckeeViewController.swift
+++ Skeleton/ViewController/AckeeViewController.swift
@@ -12,17 +12,17 @@ import EtherKit
 
 // MARK: notes
 // - EtherKit is still in development. Currently its unusable in production.
+// - The service Infura.io provides an ethereum node accessible over https, We can use the API Key EcrxWYU15S7Yu5vJNDzX , e.g access the Ropsten testnet using the url https://ropsten.infura.io/EcrxWYU15S7Yu5vJNDzX
+//    Unfortunatelly, Infura doesnt adhere to the JSONRPC spec (doesnt support String id's), so it cant be used with the current version of EtherKit
+// - We can work against a local ethereum node, run e.g. `geth --testnet --rpc --rpcaddr "127.0.0.1" --rpcport "8545" console`, then access it at http://localhost:8545
+//    I couldnt get it to work over https, so NSAppTransportSecurity/NSAllowsArbitraryLoads has been set in the info.plist for now
+
 // - EtherKit known issues:
+//   - When using EtherKit over http, geth seems to require Content-Type application/json header (else returns 415). Edit URLRequestManager.swift queueRequest method to add the header (we should make a PR into EtherKit)
 //   - EtherKit.request method has an error callback, but if an error occurs,
 //      it gets lost in the underlying URLRequestManager and the callback is never called
 //   - There are more potential places where a method's callback might not get called.
 // MARK: -----------
-// MARK: other (less important) notes:
-// - Ive signed up for Infura.io, which should let us test communication with the ethereum network without running a node.
-//    I signed up with petr.sima@ackee.cz, theres no password and no login screen wtf
-//    Infura API Key: EcrxWYU15S7Yu5vJNDzX , you can access e.g. the Ropsten testnet using the url https://ropsten.infura.io/EcrxWYU15S7Yu5vJNDzX
-//    Unfortunatelly, Infura doesnt adhere to the JSONRPC spec (doesnt support String id's), so it cant be used with the currect version of EtherKit
-//
 
 final class AckeeViewController: BaseViewController {
 
@@ -89,7 +89,7 @@ final class AckeeViewController: BaseViewController {
     }
 
   let etherKit = EtherKit(
-    URL(string: "https://ropsten.infura.io/EcrxWYU15S7Yu5vJNDzX")!,
+    URL(string: "http://localhost:8545")!,
     connectionMode: .http,
     applicationTag: "cz.ackee.etherkit.example"
   )
